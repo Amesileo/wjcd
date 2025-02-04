cbcdd638ea7e9eae2f1c7ddf6c77da75806c9948
George W J Heath-Collins <33731624+Amesileo@users.noreply.github.com>
2025-02-04 18:42:28 +0000

    Re-code

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
-let menuOpen = 0;
-let m = document.getElementById("menubutton");
-let nav = document.getElementsByTagName("nav");
-function menuTransition() {
-  if (menuOpen === 0) {
-    m.style.border = "var(--red) 2px solid";
-    m.style.backgroundColor = "var(--offwhite)";
-
-    for (const child of m.children) {
-      console.log((child.style.backgroundColor = "var(--red)"));
-    }
-
-    m.firstElementChild.style.transform = "translatey(16px) rotate(-45deg)";
-    m.lastElementChild.style.transform = "translatey(-16px) rotate(-45deg)";
-    m.children[1].style.transform = "rotate(45deg)";
-
-    nav[0].style.transform = "translatex(0px)";
-
-    menuOpen = 1;
-  } else {
-    m.style = "";
-
-    for (const child of m.children) {
-      child.style = "";
-
-      nav[0].style.transform = "translatex(-300px)";
-
-      menuOpen = 0;
-    }
-  }
+function goTo(loc) {
+	document.getElementById(loc).scrollIntoView();
 }
 
+const move = document.getElementById("cursorOuter");
+const move1 = document.getElementById("cursorMid");
+const move2 = document.getElementById("cursorInner");
 
+document.onpointermove = (event) => {
+	const { clientX, clientY } = event;
+	move.animate(
+		{
+			left: `${clientX}px`,
+			top: `${clientY}px`,
+		},
+		{ duration: 950, fill: "forwards" }
+	);
 
+	move1.animate(
+		{
+			left: `${clientX}px`,
+			top: `${clientY}px`,
+		},
+		{ duration: 300, fill: "forwards" }
+	);
+
+	move2.animate(
+		{
+			left: `${clientX}px`,
+			top: `${clientY}px`,
+		},
+		{ duration: 0, fill: "forwards" }
+	);
+};
+
+function cursorCol(col) {
+	move.style.borderColor = col;
+	move1.style.borderColor = col;
+	move2.style.borderColor = col;
+	move2.style.backgroundColor = col;
+}
+
+function cursorSelect() {
+	move.style.backgroundColor = "#0071e35d";
+	move.style.width = "60px";
+	move.style.height = "60px";
+	move.style.border = "0";
+}
+
+function cursorReturn() {
+	move.style.backgroundColor = "";
+	move.style.width = "";
+	move.style.height = "";
+	move.style.border = "";
+}
